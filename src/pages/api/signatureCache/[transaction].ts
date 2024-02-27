d892b2862df0f25406fa87274614b89aea90631e
Seungheon Oh <seungheon.ooh@gmail.com>
2024-02-27 14:45:05 -0600

    Add redis sentinel support

--- src/pages/api/signatureCache/[transaction].ts
+++ src/pages/api/signatureCache/[transaction].ts
@@ -1,34 +1,74 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { createClient } from "redis";
+import { Redis } from "ioredis";
 import kv from "@vercel/kv";
 
-export default async function handler(
+async function vercelKVHandler(
   req: NextApiRequest,
   res: NextApiResponse<{ [index: string]: string }>,
 ) {
   const { transaction, pkh } = req.query;
 
-  let client = undefined;
-  if (process.env.REDIS_SOURCE == "vercel") {
-    client = kv;
-  } else {
-    client = await createClient({ url: process.env.REDIS_SOURCE })
-      .on("error", (err) => console.log("Redis Client Error", err))
-      .connect();
-  }
-
-  if (!client) {
-    res.status(500);
-    return;
-  }
-
-  client = client as any; // idk how to fix this type
-
   let ret: { [index: string]: string } = {};
-  for await (const key of client.scanIterator({ MATCH: transaction + ":*" })) {
-    const signature = await client.get(key);
-    ret[key.split(":")[1]] = signature;
+  for await (const key of kv.scanIterator({ match: transaction + ":*" })) {
+    const signature = await kv.get(key);
+    ret[key.split(":")[1]] = signature as string;
   }
 
   res.status(200).json(ret);
+}
+
+async function redisHandler(
+  req: NextApiRequest,
+  res: NextApiResponse<{ [index: string]: string }>,
+) {
+  const { transaction, pkh } = req.query;
+
+  let client =
+    new Redis({
+      sentinels: [
+	{ host: process.env.REDIS_HOST, port: 26379 },
+      ],
+      name: process.env.REDIS_MASTER_NAME ?? 'mymaster',
+    })
+
+  const ret = await new Promise(res => {
+    const stream = client.scanStream({
+      match: transaction+":*",
+    });
+    let allKeys:string[] = [];
+
+    stream.on("data", (resultKeys) => {
+      resultKeys.forEach((key: string) => {
+	allKeys.push(key);
+      })
+    });
+    stream.on("end", () => {
+      res(allKeys);
+    });
+  }).then(keys =>
+    client
+      .pipeline((keys as string[]).map(x => ["get", x]))
+      .exec()
+      .then(res => {
+	if(!res) return {};
+	return res
+	  .map((x,i)=>{
+	    let r: {[index:string]:string} = {}
+	    r[(keys as string[])[i].split(":")[1]] = x[1] as string;
+	    return r;
+	  })
+	  .reduce((x,y)=>{
+	    return {...x, ...y};
+	  }, {})
+      }))
+
+  res.status(200).json(ret);
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<{ [index: string]: string }>,
+) {
+  if(process.env.REDIS_HOST === "vercel") return vercelKVHandler(req, res);
+  return redisHandler(req, res);
 }
--- src/pages/api/signatureCache/[transaction]/[pkh].ts
+++ src/pages/api/signatureCache/[transaction]/[pkh].ts
@@ -1,35 +1,51 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { createClient } from "redis";
+import { Redis } from "ioredis";
 import kv from "@vercel/kv";
 
-export default async function handler(
+async function vercelKVHandler(
   req: NextApiRequest,
   res: NextApiResponse<{ [index: string]: string }>,
 ) {
   const { transaction, pkh } = req.query;
 
-  let client;
-  if (process.env.REDIS_SOURCE == "vercel") {
-    client = kv;
-  } else {
-    client = await createClient({ url: process.env.REDIS_SOURCE })
-      .on("error", (err) => console.log("Redis Client Error", err))
-      .connect();
+  if (req.method === "GET") {
+    const signature = await kv.get(transaction + ":" + pkh);
+    res.status(200).json({ signature: signature as string });
+  } else if (req.method === "POST") {
+    const { vkeywitness } = JSON.parse(req.body);
+    await kv.set(transaction + ":" + pkh, vkeywitness);
+    res.status(200).json({});
   }
+}
 
-  if (!client) {
-    res.status(500);
-    return;
-  }
+async function redisHandler(
+  req: NextApiRequest,
+  res: NextApiResponse<{ [index: string]: string }>,
+) {
+  const { transaction, pkh } = req.query;
 
-  client = client as any; // idk how to fix this type
+  let client =
+    new Redis({
+      sentinels: [
+	{ host: process.env.REDIS_HOST, port: 26379 },
+      ],
+      name: process.env.REDIS_MASTER_NAME ?? 'mymaster',
+    })
 
   if (req.method === "GET") {
     const signature = await client.get(transaction + ":" + pkh);
-    res.status(200).json({ signature: signature });
+    res.status(200).json({ signature: signature as string });
   } else if (req.method === "POST") {
     const { vkeywitness } = JSON.parse(req.body);
     await client.set(transaction + ":" + pkh, vkeywitness);
     res.status(200).json({});
   }
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<{ [index: string]: string }>,
+) {
+  if(process.env.REDIS_HOST === "vercel") return vercelKVHandler(req, res);
+  return redisHandler(req, res);
 }
